f83f3552432244a84356f47400e93a893a42a44e
Igor Tuag~ <igortuag@gmail.com>
2020-08-03 12:56:10 -0300

    User Login

--- src/views/LoginUser.vue
+++ src/views/LoginUser.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <form @submit.prevent="register">
-      <label for="name">Name:</label>
-      <input v-model="name" type="text" name="name" value />
-
+    <form @submit.prevent="login">
       <label for="email">Email:</label>
-      <input v-model="email" type="text" email="email" value />
+
+      <input v-model="email" type="email" name="email" value />
 
       <label for="password">Password:</label>
       <input v-model="password" type="password" name="password" value />
 
-      <button type="submit" name="button">Register</button>
-      <router-link to="/login">Already have an account? Login.</router-link>
+      <button type="submit" name="button">Login</button>
+      <router-link to="/register">Don't have an account? Register.</router-link>
     </form>
   </div>
 </template>
@@ -20,16 +18,14 @@
 export default {
   data() {
     return {
-      name: '',
       email: '',
       password: '',
     };
   },
   methods: {
-    register() {
+    login() {
       this.$store
-        .dispatch('register', {
-          name: this.name,
+        .dispatch('login', {
           email: this.email,
           password: this.password,
         })
@@ -41,4 +37,5 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
